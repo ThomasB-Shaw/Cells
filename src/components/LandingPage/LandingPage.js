c47519a3f55fa7b06f08e0f258c6397a6d3ebffe
Thomas Brookshaw <tbrookshaw13@gmail.com>
2020-11-16 16:05:47 -0600

    Added Account Saga and Reducer, Needs Hook up to backend

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -12,24 +12,26 @@ class LandingPage extends Component {
   };
 
   componentDidMount = () => {
+    this.props.dispatch({type: 'FETCH_PAINTINGS'});
     this.getPaintings();
   }
 
   getPaintings = () => {
-    this.props.dispatch({type: 'FETCH_PAINTINGS'})
+    this.props.dispatch({type: 'FETCH_PAINTINGS'});
+    console.log(this.props.store.paintings);
   }
 
   render() {
     return (
       <div className="container">
         <h2>{this.state.heading}</h2>
-            {/* <ul>
-              {this.props.store.paintings.map(painting => {
+            <ul>
+              {/* {this.props.store.paintings.map(painting => {
                 return <li key={painting.id}>
                   <img alt={painting.description} src={painting.image_url}/>
                   </li>
-              })}
-            </ul> */}
+              })} */}
+            </ul>
             {JSON.stringify(this.props.store.paintings)}
       </div>
     );
