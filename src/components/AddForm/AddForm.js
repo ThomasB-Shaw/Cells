d629156e1088f2f796f80254e673c06f3dcc036d
Thomas Brookshaw <tbrookshaw13@gmail.com>
2020-11-29 09:17:21 -0600

    AWS Sanitized

--- src/components/AddForm/AddForm.js
+++ src/components/AddForm/AddForm.js
@@ -9,8 +9,8 @@ const config = {
     bucketName: 'solocells-images',
     dirName: 'photos', /* optional */
     region: 'us-east-2',
-    accessKeyId: 'AKIAJGC2QS2JBK2BDBKQ',
-    secretAccessKey: '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',
+    accessKeyId: `${process.env.ACCESS_KEY_ID}`,
+    secretAccessKey: `${process.env.SECRET_ACCESS_KEY}`,
 }
 
 class AddForm extends Component {
@@ -81,12 +81,30 @@ class AddForm extends Component {
               <Input type='select' name='size' id='size_type' value={this.props.state.size_type} onChange={(event) => this.props.handleChange(event, 'size_type')}>
               <option value=''></option>
               <option value='5x5'>5x5</option>
+              <option value='5x7'>5x7</option>
+              <option value='8x8'>8x8</option>
               <option value='8x10'>8x10</option>
+              <option value='10x10'>10x10</option>
               <option value='10x20'>10x20</option>
+              <option value='11x24'>11x24</option>
+              <option value='12x12'>12x12</option>
               <option value='12x24'>12x24</option>
+              <option value='12x36'>12x36</option>
               <option value='15x15'>15x15</option>
               <option value='15x30'>15x30</option>
+              <option value='16x16'>16x16</option>
+              <option value='16x20'>16x20</option>
+              <option value='18x24'>18x24</option>
+              <option value='20x20'>20x20</option>
+              <option value='22x28'>22x28</option>
+              <option value='24x24'>24x24</option>
+              <option value='24x30'>24x30</option>
+              <option value='24x36'>24x36</option>
+              <option value='24x48'>24x48</option>
+              <option value='30x30'>30x30</option>
+              <option value='30x40'>30x40</option>
               <option value='36x48'>36x48</option>
+              <option value='48x48'>48x48</option>
               <option value='48x60'>48x60</option>
               </Input>
           </FormGroup>
